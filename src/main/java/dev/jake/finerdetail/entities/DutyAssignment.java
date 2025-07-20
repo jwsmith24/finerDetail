f92080d34efb4b73ed8ad020d8026f1005e047fa
jake <jsmith181997@gmail.com>
2025-07-20 15:08:18 -0500

    add readme

---
+++ src/main/java/dev/jake/finerdetail/entities/DutyAssignment.java
@@ -0,0 +1,58 @@
+package dev.jake.finerdetail.entities;
+
+import dev.jake.finerdetail.util.constants.DetailType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+
+/**
+ * The DutyAssignment object represents a given duty that will need to be filled via polling the roster.
+ * It captures all the necessary information about the specific duty and will be tracked
+ */
+
+@Entity
+public class DutyAssignment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private LocalDate date;
+
+    @Enumerated(EnumType.STRING)
+    private DetailType detailType;
+
+    private String description; // concise & specific information for the individual detail
+
+    protected DutyAssignment() {}
+
+    public DutyAssignment(LocalDate date, DetailType detailType) {
+        this.date = date;
+        this.detailType = detailType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public DetailType getDetailType() {
+        return detailType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[ %s on %s | %s", this.detailType, this.date, "TBD");
+    }
+}
