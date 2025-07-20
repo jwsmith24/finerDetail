a7ecf39ff2fc13104aba36e8bdfba53b5be1faf0
jake <jsmith181997@gmail.com>
2025-07-20 13:22:11 -0500

    initial commit

---
+++ src/main/java/dev/jake/finerdetail/FinerDetailApplication.java
@@ -0,0 +1,13 @@
+package dev.jake.finerdetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinerDetailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinerDetailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jake/finerdetail/FinerDetailApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jake.finerdetail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinerDetailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
